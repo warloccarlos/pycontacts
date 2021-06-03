9b95cc7db54f96ee478892a6630b7f769c596e56
Warloc Carlos <warloccarlos@gmail.com>
2021-06-03 10:25:11 +0530

    Changes made after security run

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@
 import mysql.connector
 import config as cfg
 
-class cfgu:
+class ConfigMain:
     global fname, lname, company, mobile, email
 
     def saveDetails(self):
@@ -17,15 +17,14 @@ class cfgu:
             password=cfg.sql['password'], database=cfg.sql['database'], port=cfg.sql['port'])
             cur = con.cursor()
             val = (self.fname, self.lname, self.company, self.mobile, self.email)
-            query = "insert into connections(fname, lname, company, mobile, email) values(%s, %s, %s, %s, %s);"
+            query = "insert into connections(fname, lname, company, mobile, email)\
+             values(%s, %s, %s, %s, %s);"
             cur.execute(query, val)
             con.commit()
             if cur.rowcount ==1:
                 print("Details saved.")
-                return 1
             else:
                 print("Details not saved.")
-                return 0
             con.close()
         except Exception as e:
             print(e)
@@ -36,8 +35,8 @@ class cfgu:
             password=cfg.sql['password'], database=cfg.sql['database'], port=cfg.sql['port'])
             cur = con.cursor()
             name = input("First Name that you want to delete: ")
-            query = "delete from connections where fname='"+name+"';"
-            cur.execute(query)
+            query = "delete from connections where fname=%s;"
+            cur.execute(query, (name,))
             con.commit()
             if cur.rowcount == 1:
                 print("Detail deleted.")
@@ -55,10 +54,24 @@ class cfgu:
             password=cfg.sql['password'], database=cfg.sql['database'], port=cfg.sql['port'])
             cur = con.cursor()
             name = input("First Name that you want to search: ")
-            query = "select * from connections where fname='"+name+"';"
-            cur.execute(query)
+            query = "select * from connections where fname=%s;"
+            cur.execute(query, (name,))
             rs = cur.fetchone()
             print(rs)
+            con.close()
+        except Exception as e:
+            print(e)
+
+    def viewAll(self):
+        try:
+            con = mysql.connector.connect(host=cfg.sql['host'], user=cfg.sql['user'], \
+            password=cfg.sql['password'], database=cfg.sql['database'], port=cfg.sql['port'])
+            cur = con.cursor()
+            query = "select * from connections;"
+            cur.execute(query)
+            rs = cur.fetchall()
+            for r in rs:
+                print(r)
             con.close()
         except Exception as e:
             print(e)
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 #! /usr/bin/python
 
+import sys
 from configuration import cfgu
 
-c = cfgu()
+c = ConfigMain()
 
-print("Welcome to your Contacts App to\
+print("Welcome to your Contacts App \n\
  Connect with people you know")
-act = input("N. New Contact     D. Delete Contact	S. Search Contact     0. Exit  ")
+act = input("N. New Contact     D. Delete Contact	S. Search Contact     V. View All     0. Exit  ")
 if act=='N':
     c.saveDetails()
 elif act=='D':
     c.delDetails()
 elif act=='S':
     c.searchDetails()
+elif act=='V':
+    c.viewAll()
 elif act=='0':
-    exit(0)
+    print('See ya later!')
+    sys.exit(0)
 else:
     print('You chose wrong')
