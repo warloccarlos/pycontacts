06a7ab608c1358be21d475ab788ed44d718f8cd1
Warloc Carlos <warloccarlos@gmail.com>
2021-06-03 10:33:09 +0530

    Some changes after security runs and other UI changes

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 #! /usr/bin/python
 
 import sys
-from configuration import cfgu
+from configuration import ConfigMain
 
 c = ConfigMain()
 
 print("Welcome to your Contacts App \n\
  Connect with people you know")
-act = input("N. New Contact     D. Delete Contact	S. Search Contact     V. View All     0. Exit  ")
-if act=='N':
-    c.saveDetails()
-elif act=='D':
-    c.delDetails()
-elif act=='S':
-    c.searchDetails()
-elif act=='V':
-    c.viewAll()
-elif act=='0':
-    print('See ya later!')
-    sys.exit(0)
-else:
-    print('You chose wrong')
+
+def base():
+    act = input("N. New Contact     D. Delete Contact	S. Search Contact     V. View All     0. Exit  ")
+    if act=='N':
+        c.saveDetails()
+        base()
+    elif act=='D':
+        c.delDetails()
+        base()
+    elif act=='S':
+        c.searchDetails()
+        base()
+    elif act=='V':
+        c.viewAll()
+        base()
+    elif act=='0':
+        print('See ya later!')
+        sys.exit(0)
+    else:
+        print('You chose wrong')
+        choice = input('Would you like to try again? Y/N: ')
+        if choice == 'Y':
+            base()
+        else:
+            sys.exit(0)
+
+base()
